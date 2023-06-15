c55369f781e63f32c88af243dd373c340cd6e98c
Aaron <aaturner1995@gmail.com>
2023-06-14 21:35:17 -0300

    added apollo queries and mutations on the client

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,25 +3,26 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const AppNavbar = () => {
-    return (
-        <>
-            <Navbar bg='dark' variant='dark' expand='lg'>
-                <Container fluid>
-                    <Navbar.Brand as={Link} to='/'>
-                        Project 3
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" className='mx-5' />
-                    <Navbar.Collapse id="basic-navbar-nav" className="nav-icons mx-5">
-                        <Nav className='ml-auto d-flex'>
-                            <Nav.Link as={Link} to='/'>
-                                Link
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    )
-}
+  return (
+    <Container fluid>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+        <Navbar.Brand as={Link} to='/'>
+          Project 3
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className='mx-5' />
+        <Navbar.Collapse id="basic-navbar-nav" className="nav-icons mx-5">
+          <Nav className='ml-auto d-flex'>
+            <Nav.Link as={Link} to='/'>
+              Link
+            </Nav.Link>
+            <Nav.Link as={Link} to='/'>
+              Link
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
+};
 
-export default AppNavbar
+export default AppNavbar;
