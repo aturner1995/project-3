03ce611e83b7885d970cb3e7f35e6d343e827886
Matt Zhao <mattzhao@MP.ht.home>
2023-06-21 21:21:21 -0300

    added user profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Profile = () => {
+  return <div>{/* profile content to be provided */}</div>;
+};
+
+export default Profile;
