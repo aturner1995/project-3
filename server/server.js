8fd66babc0d8f82e564ea5928ceaee752edba480
Aaron <aaturner1995@gmail.com>
2023-06-28 12:18:31 -0300

    heroku fix

--- server/server.js
+++ server/server.js
@@ -21,12 +21,11 @@ app.use(express.json({ limit: '50mb' }));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Return index.html for all routes except '/graphql'
+  app.get(/^(?!\/graphql).*$/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
